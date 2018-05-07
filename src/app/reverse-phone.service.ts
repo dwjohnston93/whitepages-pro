bb3ed36fecbcf1774b322e7c5bdc0c39532db1b3
dwjohnston93 <dwjohnston93@gmail.com>
2018-05-07 18:26:43 +0000

    everything working, no styling and only displaying carrier

--- src/app/reverse-phone.service.ts
+++ src/app/reverse-phone.service.ts
@@ -8,17 +8,20 @@ export class ReversePhoneService {
  //template url: string = "https://proapi.whitepages.com/3.0/phone?phone=2061115201&api_key=API_KEY";
  phone: string = "phone?phone="
  myKey: string = "036aaec05d7545efad76ba76088998cd";
- url: string = "https://proapi.whitepages.com/3.0"
+ url: string = "http://proapi.whitepages.com/3.0/"
 apiKey: string ="&api_key=036aaec05d7545efad76ba76088998cd"
-
+  /*
+  We want this format
+  https://proapi.whitepages.com/3.0/phone?phone=8312775921api_key=ebb50680ac20482bbe25eec09af00d4c
+  */
  constructor(private _http: HttpClient) {
     
  }
  
     getData (phoneNum){
-        console.log("my url request", this.url + this.phone + phoneNum+ this.apiKey);
+        console.log("my url request", (this.url + this.phone + phoneNum + this.apiKey));
         let urlRequest = this.url + this.phone + phoneNum + this.apiKey; 
         return this._http.get(urlRequest)
-        }
+       }
  
 } 
